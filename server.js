564b2356b993b63a2c80fd17f75e74d085958a8c
Karsten Taylor <karstentaylor0@gmail.com>
2020-07-21 09:21:59 -0700

    security

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIES = require('./movies-data-small.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(cors());
 app.use(helmet());
 
@@ -24,28 +25,36 @@ app.get('/movie', function handleGetMovie(req, res) {
   let response = MOVIES;
 
   if (req.query.genre) {
-    response = response.filter(movie =>
+    response = response.filter((movie) =>
       movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
     );
   }
 
   if (req.query.country) {
-    response = response.filter(movie =>
+    response = response.filter((movie) =>
       movie.country.toLowerCase().includes(req.query.country.toLowerCase())
     );
   }
 
   if (req.query.avg_vote) {
-    response = response.filter(movie =>
-      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    response = response.filter(
+      (movie) => Number(movie.avg_vote) >= Number(req.query.avg_vote)
     );
   }
 
   res.json(response);
 });
 
-const PORT = 9000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => {});
